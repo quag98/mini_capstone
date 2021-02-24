37417b0385e0b26ee5d6d6534cf79af4d1ffc355
David Quaggin-Smith <quagginsmith@gmail.com>
2021-02-24 14:00:51 -0600

    adds Product.create in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create([{ name: "Bike", price: 300, image_url: "https://cdn.shopify.com/s/files/1/0021/5701/9254/products/BBIAA35_FX-ADV_-FORCE-CX1-AQUA-rgb72dpi_d7c23c68-b57e-499c-8641-77fc6848981b.jpg?v=1597187785", description: "A blue road bike" }])
